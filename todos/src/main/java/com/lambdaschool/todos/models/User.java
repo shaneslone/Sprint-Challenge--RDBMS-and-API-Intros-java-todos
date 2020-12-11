e317c394d25afc1e89e3d82c1768bbf635d53f0c
Shane Slone <slone.shane@gmail.com>
2020-12-11 12:43:26 -0500

    finished mvp

--- todos/src/main/java/com/lambdaschool/todos/models/User.java
+++ todos/src/main/java/com/lambdaschool/todos/models/User.java
@@ -5,8 +5,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * The entity allowing interaction with the users table
@@ -43,6 +43,10 @@ public class User extends Auditable
         unique = true)
     @Email
     private String primaryemail;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnoreProperties(value = "user", allowSetters = true)
+    private Set<Todos> todos = new HashSet<>();
 
     /**
      * Default constructor used primarily by the JPA.
@@ -160,5 +164,13 @@ public class User extends Auditable
     public void setPassword(String password)
     {
         this.password = password;
+    }
+
+    public Set<Todos> getTodos() {
+        return todos;
+    }
+
+    public void setTodos(Set<Todos> todos) {
+        this.todos = todos;
     }
 }
